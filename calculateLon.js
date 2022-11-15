698302de0c4ccb300b6c4f85a0624f6589ad571c
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2022-11-14 20:25:46 -0500

    second commit

--- calculateLon.js
+++ calculateLon.js
@@ -1,8 +1,18 @@
 const startDayInput = document.querySelector('#startDate');
 const aptDayInput = document.querySelector('#endDate');
 const aptTimeInput = document.querySelector('#aptTime');
+const lonContainer = document.querySelector('#lon-container')
+const includeWeekend = document.querySelector('#includeWeekend')
 
 const submitBtn = document.querySelector('#submitBtn');
+
+includeWeekend.addEventListener("click", ()=> {
+    if (includeWeekend.value === "true") {
+        includeWeekend.value = "false"
+    } else {
+        includeWeekend.value = "true"
+    }
+})
 
 submitBtn.addEventListener("click", getDaysNotice)
 
@@ -13,20 +23,38 @@ const oneDay = 1000 * 60 * 60 * 24
 
 
 function getDaysNotice (event){
-    event.preventDefault()
-    //assgined date from input to sday and removing hyphens in order to show the correct date 
-    const startDay = new Date(startDayInput.value.replace(/-/g, '\/'))
-    const aptDay = new Date(aptDayInput.value.replace(/-/g, '\/'))
-    
+    event.preventDefault() 
+
+    //create new date from input 
+    const startDay = new Date(startDayInput.value)
+    const aptDay = new Date(aptDayInput.value)
+
+    //calculate the difference in milliseconds 
     let differenceMs = aptDay.getTime() - startDay.getTime();
 
+    //get days between dates 
+    let daysNotice = differenceMs/oneDay;
+    //get hours between dates 
+    let hoursNotice = differenceMs/oneDay * 24;
 
-    return console.log( Math.round(differenceMs/oneDay))
+    if (includeWeekend.value === "true") {
+        hoursNotice = hoursNotice - 48;
+        daysNotice = daysNotice - 2;
+
+        console.log(hoursNotice)
+    }
+
+    //create new element 
+    const hoursNoticeDiv = document.createElement("p")
+    const daysNoticeDive  = document.createElement("p")
+
+
+    hoursNoticeDiv.innerHTML = `${hoursNotice.toFixed(2)} hours days notice | ${daysNotice} business days`
+    lonContainer.appendChild(hoursNoticeDiv)
+    
+    return console.log(daysNotice.toFixed(2), hoursNotice.toFixed(2))
 }
 
-// const calculateLon = (startDay, endDay) => {
-    
-// }
 
 
-console.log(currentTime)
+console.log(includeWeekend.value)
